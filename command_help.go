0c7eda8c47f279b1f3006725c3d45ec7c989598c
gioadorno <adorno.gio@gmail.com>
2024-06-22 20:38:05 -0700

    Update function to return nil for error

--- command_help.go
+++ command_help.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-func callbackHelp() {
+func callbackHelp() error {
 	fmt.Println("Welcome to the Pokedex help menu!")
 	fmt.Println("Here are your available commands:")
 
 	availableCommands := getCommands()
 	for _, cmd := range availableCommands {
-		fmt.Printf(" - %s: %s", cmd.name, cmd.description)
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
 	}
 
 	fmt.Println("")
+	return nil
 }
