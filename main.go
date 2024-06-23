363111c113e90284fca6a1e8a70c444159cc2ef6
gioadorno <adorno.gio@gmail.com>
2024-06-23 16:20:43 -0700

    Update main

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "github.com/gioadorno/pokidexcli/internal/pokeapi"
+
+type config struct {
+  pokeapiClient pokeapi.Client
+  nextLocationAreaURL *string
+  prevLocationAreaURL *string
+}
+
 func main()  {
- startRepl() 
+  cfg := config{
+    pokeapiClient: pokeapi.NewClient(),
+  }
+
+
+ startRepl(&cfg) 
 }
