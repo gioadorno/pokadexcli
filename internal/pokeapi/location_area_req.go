95e363f0fabaf252f93ad91aa6a5e42d7ab771fe
gioadorno <adorno.gio@gmail.com>
2024-06-23 20:12:42 -0700

    Implement caching on request

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -8,39 +8,55 @@ import (
 )
 
 func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
-  endpoint := "/location-area"
-  fullURL := baseURL + endpoint
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
 
-  if pageURL != nil {
-    fullURL = *pageURL
-  }
+	if pageURL != nil {
+		fullURL = *pageURL
+	}
 
- req, err :=  http.NewRequest("GET", fullURL, nil)
-  if err != nil {
-    return LocationAreasResp{}, err
-  }
+	// check the cache
+	dat, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit
+    fmt.Println("cache hit!")
+		locationAreasResp := LocationAreasResp{}
+		err := json.Unmarshal(dat, &locationAreasResp)
+		if err != nil {
+			return LocationAreasResp{}, err
+		}
 
-  resp, err := c.httpClient.Do(req)
-  if err != nil {
-    return LocationAreasResp{}, err
-  }
-  defer resp.Body.Close()
+		return locationAreasResp, nil
+	}
+    fmt.Println("cache miss!")
 
-  if resp.StatusCode > 399 {
-    return LocationAreasResp{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
-  }
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
 
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+	defer resp.Body.Close()
 
-  data, err := io.ReadAll(resp.Body)
-  if err != nil {
-    return LocationAreasResp{}, err
-  }
+	if resp.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
+	}
 
-  locationAreasResp := LocationAreasResp{}
-  err = json.Unmarshal(data, &locationAreasResp)
-  if err != nil {
-    return LocationAreasResp{}, err
-  }
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
 
-  return locationAreasResp, nil
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(data, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+  c.cache.Add(fullURL, data)
+
+	return locationAreasResp, nil
 }
