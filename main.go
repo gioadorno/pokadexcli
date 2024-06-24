0e3a39037aebb37aab3ab6618b603700fed9dfa7
gioadorno <adorno.gio@gmail.com>
2024-06-23 20:11:42 -0700

    Add interval to main client

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gioadorno/pokidexcli/internal/pokeapi"
+import (
+	"time"
+
+	"github.com/gioadorno/pokidexcli/internal/pokeapi"
+)
 
 type config struct {
   pokeapiClient pokeapi.Client
@@ -10,7 +14,7 @@ type config struct {
 
 func main()  {
   cfg := config{
-    pokeapiClient: pokeapi.NewClient(),
+    pokeapiClient: pokeapi.NewClient(time.Hour),
   }
 
 
