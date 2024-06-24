8faa83cd6402d2c2ab9f721d5dbfd4db5de448f7
gioadorno <adorno.gio@gmail.com>
2024-06-23 19:40:43 -0700

    Implement more cache tests

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -3,8 +3,44 @@ package pokecache
 import "testing"
 
 func TestCreateCache(t *testing.T) {
-  cache := NewCache()
-  if cache.cache == nil{
-    t.Error("cache is nil")
-  }
+	cache := NewCache()
+	if cache.cache == nil {
+		t.Error("cache is nil")
+	}
+}
+
+func TestAddCache(t *testing.T) {
+	cache := NewCache()
+
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
+	}
+
+	for _, cas := range cases {
+		cache.Add(cas.inputKey, cas.inputVal)
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%s not found", cas.inputKey)
+			continue
+		}
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%s does not match %s", string(actual), string(cas.inputVal))
+			continue
+		}
+	}
+
 }
