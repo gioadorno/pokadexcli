3f09fa54c95d799d900a911b4982e2dccdcc8171
gioadorno <adorno.gio@gmail.com>
2024-06-29 21:19:53 -0700

    Update inspect

--- command_inspect.go
+++ command_inspect.go
@@ -19,9 +19,11 @@ func callbackInspect(cfg *config, args ...string) error {
 	fmt.Printf("Name: %s\n", pokemon.Name)
 	fmt.Printf("Height: %v\n", pokemon.Height)
 	fmt.Printf("Weight: %v\n", pokemon.Weight)
+  fmt.Println("Stats:")
 	for _, stat := range pokemon.Stats {
 		fmt.Printf(" - %s: %v\n", stat.Stat.Name, stat.BaseStat)
 	}
+  fmt.Println("Types:")
 	for _, typ := range pokemon.Types {
 		fmt.Printf(" - %s", typ.Type.Name)
 	}
