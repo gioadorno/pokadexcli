e8dbd99b42a9f8aed687111b759e92cfb49e05f9
gioadorno <adorno.gio@gmail.com>
2024-06-29 20:35:10 -0700

    Update map

--- command_map.go
+++ command_map.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func callbackMap(cfg *config) error {
+func callbackMap(cfg *config, args ...string) error {
 	resp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
 	if err != nil {
 		return err
@@ -20,7 +20,7 @@ func callbackMap(cfg *config) error {
 	return nil
 }
 
-func callbackMapB(cfg *config) error {
+func callbackMapB(cfg *config, args ...string) error {
 	if cfg.prevLocationAreaURL == nil {
 		errors.New("You're on the first page")
 	}
